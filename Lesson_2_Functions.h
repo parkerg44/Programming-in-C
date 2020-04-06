437f2c7bd0e7cdff42173d9548c1325b28184218
parkerg44 <56697192+parkerg44@users.noreply.github.com>
2020-04-06 16:27:39 -0700

    Update and rename equations.h to Lesson_2_Functions.h

--- Lesson_2_Functions.h
+++ Lesson_2_Functions.h
@@ -1,14 +1,16 @@
 /* Author: Parker Guillen
 1/30/17
-Description: PA 2, header file, function prototypes are listed along with include and define statement.
+Description: Lesson 2, header file, function prototypes are listed along with include and define statement.
 */         
 
+//it is good practice to place all includes/defines in the header file
 #include <stdio.h> //printf() and scanf()
 #include <math.h>
 #define PI 3.14159	// Defines PI as 3.14159
 #define G 6.67e-11	// Defines G 6.67e-11
 
-// Function prototypes 
+// Function prototypes: These are the names of the functions within the source file 
+// if using the three file format the function prototypes must first be defined in the header
 double calculate_newtons_2nd_law(double mass, double accel);
 double calculate_volume_cylinder(double radius, double height);
 char perform_character_encoding(char plaintext);
@@ -17,3 +19,4 @@ double calculate_resistive_divider(double r1, double r2, double vin);
 double calculate_distance_between_2pts(double x1, double x2, double y1, double y2);
 double calculate_general_equation(int a, double x, double z);
 
+//End of Header File
